c1da6287a3295f598eee455b49a128c3ac585c55
Julio Faria <julio-pinheiroo@hotmail.com>
2021-11-16 19:43:00 -0300

    Problema de acento corrigido

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const server = http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.end('Ola Mundo querido! Meu nome é Júlio Pinheiro Chagas de Faria')
+  res.end('Ola Mundo querido! Meu nome &eacute; J&uacute;lio Pinheiro Chagas de Faria')
 })
 
 const port = (process.env.PORT || 3000)
