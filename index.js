ed5c816e9da184cffe4033ceeea16bdae87b33c8
Julio Faria <julio-pinheiroo@hotmail.com>
2021-11-16 21:05:25 -0300

    charset inserido no cabeçalho

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http')
 
 const server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.end('Ola Mundo querido! Meu nome &eacute; J&uacute;lio Pinheiro Chagas de Faria')
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8;' })
+  res.end('Ola Mundo querido! Meu nome é Júlio Pinheiro Chagas de Faria')
 })
 
 const port = (process.env.PORT || 3000)
